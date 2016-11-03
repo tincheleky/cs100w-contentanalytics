76776b786ad7f6c9f0391fa9c3a2038dcaab8e4b
Tin Che <tin.che0301@gmail.com>
2016-11-03 23:35:11 +0000

    Edited

--- db/migrate/20161103223114_change_col_names.rb
+++ db/migrate/20161103223114_change_col_names.rb
@@ -1,7 +1,15 @@
 class ChangeColNames < ActiveRecord::Migration[5.0]
   def change
     rename_column :ads, :time, :ad_time
-    rename_column :ads, :ad_rul, :ad_url
+    rename_column :ads, :url, :ad_url
+    rename_column :ads, :type, :ad_type
+    rename_column :ads, :name, :ad_name
+    rename_column :ads, :duration, :ad_duration
+    rename_column :ads, :description, :ad_description
+    rename_column :ads, :display_id, :ad_display_id
+
+
+    
 
   end
 end
